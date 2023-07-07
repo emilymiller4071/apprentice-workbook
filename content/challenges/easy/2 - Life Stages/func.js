150575dcde24774b9cfbad19e367e7b1d2efe33c
Emily Miller <emilyrauch3@gmail.com>
2023-07-07 12:31:09 -0400

    exercise 2 complete

--- content/challenges/easy/2 - Life Stages/func.js
+++ content/challenges/easy/2 - Life Stages/func.js
@@ -7,4 +7,20 @@
 // Greater than 19 and less than 30 is 'Young Adult'
 // Greater than 30 is 'Dead'
 module.exports = (age) => {
+
+    switch(true) {
+        case (age < 2): return 'Baby';            
+            break;
+        case (age >= 2 && age < 13): return 'Kid';
+            break;
+        case (age >= 13 && age <= 19): return 'Teen';
+            break;
+        case (age > 19 && age <=30): return 'Young Adult';
+            break;
+        case (age > 30): return 'Dead';
+            break;
+        default: return 'Unknown';
+    }
 }
+
+
