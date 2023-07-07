9dc1034c7204bce46f637ddbf9cebf2ef4b4985d
Emily Miller <emilyrauch3@gmail.com>
2023-07-07 19:47:06 -0400

    exercise 6 complete

--- content/challenges/easy/6 - Flip the Boolean/func.js
+++ content/challenges/easy/6 - Flip the Boolean/func.js
@@ -1,3 +1,12 @@
 // Create a function that reverses a boolean value and returns the string "boolean expected" if another variable type is given
 module.exports = (bool) => {
+    if (bool === true) {
+        bool = !bool;
+        return bool;
+    }else if (bool === false) {
+        bool = !bool;
+        return bool;
+    } else {
+        return 'boolean expected';
+    }
 };
