3723941a8106765276b4f812b3d1b3016a5e58a2
Emily Miller <emilyrauch3@gmail.com>
2023-07-07 12:08:16 -0400

    exercise 1

--- content/challenges/easy/1 - Before Noon/func.js
+++ content/challenges/easy/1 - Before Noon/func.js
@@ -1,4 +1,58 @@
 // Check if the time is before noon
 // time is in military time 0-24
+
+
 module.exports = (hour) => {
+    let beforeNoon;
+    if (hour < 12) {
+        beforeNoon = true;
+        console.log('It is before noon.')
+        return beforeNoon;
+    } else if (hour > 12) {
+        beforeNoon = false;
+        console.log('It is afternoon.')
+        return beforeNoon
+    }else if (hour === 12) {
+        beforeNoon = false;
+        console.log('It is noon.')
+        return beforeNoon
+    }
 }
+
+// Testing times to verify output
+
+console.log(module.exports(0));
+console.log(module.exports(12));
+console.log(module.exports(22));
+
+
+/*
+
+This gets the current time, and pads minutes if they are in the single digits.
+
+
+
+module.exports = () => { 
+    const hour = new Date().getHours();
+    const minutes = new Date().getMinutes().toString().padStart(2, '0');
+    const currentTime = `The time is currently ${hour}:${minutes}.`;
+    let beforeNoon;
+
+    if (hour < 12) {
+        beforeNoon = true;
+        console.log(currentTime + ' It is before noon.');
+        return beforeNoon;
+    } else if (hour > 12) {
+        beforeNoon = false;
+        console.log(currentTime + ' It is after noon.');
+        return beforeNoon;
+    } else if (hour === 12) {
+        beforeNoon = false;
+        console.log(currentTime + 'It is noon.');
+        return beforeNoon;
+    } 
+}
+
+console.log(module.exports());
+
+*/
