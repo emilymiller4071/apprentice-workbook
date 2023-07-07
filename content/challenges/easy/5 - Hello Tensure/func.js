8aa70e7aceb2fc7c42a9de21f1ec72c27474e14b
Emily Miller <emilyrauch3@gmail.com>
2023-07-07 19:35:24 -0400

    exercise 5 complete

--- content/challenges/easy/5 - Hello Tensure/func.js
+++ content/challenges/easy/5 - Hello Tensure/func.js
@@ -1,4 +1,18 @@
 // if the number is a multiple of 3, return "Hello"
 // if the number is a multiple of 5, return "Tensure"
 // if the number is a multiple of both 3 and 5, return "Hello Tensure"
-module.exports = (num) => {};
+module.exports = (num) => {
+
+    if (num % 3 === 0) {
+        if (num % 5 === 0){
+            return 'Hello Tensure';
+        } else {
+            return 'Hello';
+        }
+    } else if (num % 5 === 0) {
+        return 'Tensure';
+    } else {
+        return 'Not a multiple of 3 or 5';
+    }
+};
+
