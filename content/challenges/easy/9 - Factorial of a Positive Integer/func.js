1c89faaa3ffc88324205d7a8ad39afee9d24dd96
Emily Miller <emilyrauch3@gmail.com>
2023-07-09 15:34:29 -0400

    exercise 9 complete

--- content/challenges/easy/9 - Factorial of a Positive Integer/func.js
+++ content/challenges/easy/9 - Factorial of a Positive Integer/func.js
@@ -2,4 +2,12 @@
    The factorial of 0 is 1.
    The factorial of any positive integer Z is Z * (Z - 1) * (Z - 2) * . . . . . . * 1 (e.g. factorial of 3 is 3 * 2 * 1 = 6). */
 module.exports = (num) => {
+   let factorial = 1;
+
+   for (let subFromNum = num; subFromNum >= 1; subFromNum--) {
+      factorial *= subFromNum;
+   }
+   return factorial;
 };
+
+console.log(module.exports(3));
