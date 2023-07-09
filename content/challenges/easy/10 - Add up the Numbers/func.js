93b8e2400adfc1731bf5249989b475f399c8b23d
Emily Miller <emilyrauch3@gmail.com>
2023-07-09 15:41:44 -0400

    exercise 10 complete

--- content/challenges/easy/10 - Add up the Numbers/func.js
+++ content/challenges/easy/10 - Add up the Numbers/func.js
@@ -1,5 +1,13 @@
-/* Create a function that takes a number as an argument. Add up all the numbers from 1 to the number you passed to the function. For example, if the input is 4 then your function should return 10 because 1 + 2 + 3 + 4 = 10. USE FOR LOOP */
+/* Create a function that takes a number as an argument. 
+Add up all the numbers from 1 to the number you passed to the function. 
+For example, if the input is 4 then your function should return 10 because 1 + 2 + 3 + 4 = 10. 
+USE FOR LOOP */
 
 module.exports = (num) => {
-
+    let result = 0;
+    
+    for (count = 1; count <= num; count++) {
+        result += count;
+    }
+    return result;
 };
