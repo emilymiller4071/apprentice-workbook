5b1f9c63fc1aa5a1fa16225508347f73a05a9cb1
Emily Miller <emilyrauch3@gmail.com>
2023-07-09 16:20:19 -0400

    exercise M1 complete

--- content/challenges/medium/1 - Double Letter/func.js
+++ content/challenges/medium/1 - Double Letter/func.js
@@ -1,3 +1,11 @@
-// Create a function that takes a word and returns true if the word has two consecutive identical letters.
+// Create a function that takes a word and returns true if the word has two 
+//consecutive identical letters.
 module.exports = (word) => {
+
+    for (let checkLetter = 0; checkLetter < word.length; checkLetter++) {
+        if (word[checkLetter] === word[checkLetter + 1]) {
+            return true;
+        }
+    }
+    return false;
 }
