c4e2ea10700b332339f02a6b16c0c5fdbb443759
Emily Miller <emilyrauch3@gmail.com>
2023-07-09 22:45:20 -0400

    exercise M2 complete

--- content/challenges/medium/2 - Missing Number/func.js
+++ content/challenges/medium/2 - Missing Number/func.js
@@ -1,6 +1,14 @@
-// Create a function that takes an array of numbers between 1 and 10 (excluding one number) and returns the missing number.
+// Create a function that takes an array of numbers between 1 and 10 (excluding one number) 
+// and returns the missing number.
 // Notes:
 //   - The array of numbers will be unsorted (not in order).
 //   - Only one number will be missing.
 module.exports = (arr) => {
+    let missingNumber = 0;
+    let expectedTotal = 55;
+    
+    for (let i = 0; i < arr.length; i++) {
+        expectedTotal -= arr[i];
+    }
+    return expectedTotal - missingNumber;
 }
