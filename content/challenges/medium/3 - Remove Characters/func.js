2d5b125df405d1cc130d83b86fc8fbdce31c8df7
Emily Miller <emilyrauch3@gmail.com>
2023-07-10 12:27:09 -0400

    exercise M3 test file correction

--- content/challenges/medium/3 - Remove Characters/func.js
+++ content/challenges/medium/3 - Remove Characters/func.js
@@ -1,4 +1,14 @@
-// Create a function that takes a string, removes all "special" characters (e.g. !, @, #, $, %, ^, &, \, *, (, )) and 
-// returns the new string. The only non-alphanumeric characters allowed are dashes -, underscores _ and spaces.
+// Create a function that takes a string, removes all "special" characters 
+// (e.g. !, @, #, $, %, ^, &, \, *, (, )) and 
+// returns the new string. The only non-alphanumeric characters 
+// allowed are dashes -, underscores _ and spaces.
+
 module.exports = (str) => {
+
+    str = str.replace(/[&\/\\#,+()$~%.'":*?<>{}!@%^=]/g, '');
+    return str;
+
+    // testing criteria has a typo, that still expects the period '.'
+    // I hope that I'm not missing something in the instructions.
+    // I fixed the testing file on my own fork...
 }
