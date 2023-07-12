a30dabd3ce3b3b729ac22163189b81149014edfc
Emily Miller <emilyrauch3@gmail.com>
2023-07-11 22:04:25 -0400

    exercise M6 complete

--- content/challenges/medium/6 - Capitalize Words/func.js
+++ content/challenges/medium/6 - Capitalize Words/func.js
@@ -1,6 +1,22 @@
-// Create a function that takes a string as an argument and converts the first character of each word to uppercase. 
+// Create a function that takes a string as an argument and converts 
+// the first character of each word to uppercase. 
 // Return the newly formatted string.
 // Notes: 
 //   - You can expect a valid string for each test case.
 module.exports = (str) => {
+
+    let capitalizedString = ''; 
+    
+    for (let i = 0; i < str.length; i++) {
+        const currentChar = str[i];
+        const previousChar = i === 0 ? '' : str[i - 1];
+
+        if (i === 0 || previousChar === ' ') {
+            capitalizedString += currentChar.toUpperCase();
+        } else {
+            capitalizedString += currentChar;
+        }
+
+    }
+    return capitalizedString;
 }
