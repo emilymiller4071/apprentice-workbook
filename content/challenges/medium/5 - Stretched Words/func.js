9ec8b8885e591b64840cce3fcbaad16e82726e7d
Emily Miller <emilyrauch3@gmail.com>
2023-07-11 21:42:35 -0400

    exercise M5 complete

--- content/challenges/medium/5 - Stretched Words/func.js
+++ content/challenges/medium/5 - Stretched Words/func.js
@@ -1,6 +1,17 @@
-// Write a function that takes a string, and returns a new string with any duplicate consecutive letters removed.
+// Write a function that takes a string, and returns a new string with any 
+// duplicate consecutive letters removed.
 // Notes:
 //   - Final strings won't include words with double letters (e.g. "passing", "lottery").
 
 module.exports = (word) => {
+    let fixedWord = '';
+    
+
+    for (let letter = 0; letter < word.length; letter++) {
+        
+        if (letter === 0 || word[letter] !== word[letter - 1]) {
+            fixedWord += word[letter];
+        }
+    }
+    return fixedWord;
 }
