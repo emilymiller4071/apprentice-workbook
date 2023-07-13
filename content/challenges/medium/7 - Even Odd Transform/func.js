8f8b2dce4475787488460a8378848eb6d9aff009
Emily Miller <emilyrauch3@gmail.com>
2023-07-13 12:16:19 -0400

    exercise M7 complete

--- content/challenges/medium/7 - Even Odd Transform/func.js
+++ content/challenges/medium/7 - Even Odd Transform/func.js
@@ -3,4 +3,22 @@
 //   1. Adds two (+2) to each odd integer.
 //   2. Subtracts two (-2) to each even integer.
 module.exports = (arr, n) => {
+
+    let transformedArray = [];
+
+    for(let thisNumber = 0; thisNumber < arr.length; thisNumber++) {
+        let transformedNumber = arr[thisNumber];
+
+        for (let i = 0; i < n; i++) {
+
+            if (transformedNumber % 2 === 0) {
+                transformedNumber -= 2;
+            } else {
+                transformedNumber += 2;
+            }
+        }
+
+        transformedArray.push(transformedNumber);
+    }
+    return transformedArray;
 }
