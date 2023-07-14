9e87971cabaa9157595ccc232476ebbe401e2f47
Emily Miller <emilyrauch3@gmail.com>
2023-07-13 21:56:27 -0400

    exercise M10 complete

--- content/challenges/medium/10 - Greatest Common Denominator/func.js
+++ content/challenges/medium/10 - Greatest Common Denominator/func.js
@@ -2,5 +2,15 @@
 // Notes: 
 //   - Both values will be positive.
 //   - The GCD is the largest factor that divides both numbers.
+
 module.exports = (int1, int2) => {
+    while (int1 !== int2) {
+
+        if (int1 > int2) {
+            int1 -= int2;
+        } else {
+            int2 -= int1;
+        }
+    }
+    return int1;
 }
