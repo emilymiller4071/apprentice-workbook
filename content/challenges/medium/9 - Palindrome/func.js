895daa2189b68538039bb9f1eef38e3f5412fd4e
Emily Miller <emilyrauch3@gmail.com>
2023-07-13 21:42:43 -0400

    exercise M9 complete

--- content/challenges/medium/9 - Palindrome/func.js
+++ content/challenges/medium/9 - Palindrome/func.js
@@ -1,5 +1,23 @@
 // Write a function that determines if a string is a palindrome.
 // Notes: 
 // An empty string counts as a palindrome.
+
 module.exports = (str) => {
+    let isPalindrome = true;
+    let forwardWord = '';
+    let backwardWord = '';
+
+    for (let forwardIteration = 0; forwardIteration < str.length; forwardIteration++) {
+        forwardWord += str[forwardIteration];
+    }
+        
+    for (let reverseIteration = str.length - 1; reverseIteration >= 0; reverseIteration--) {
+            backwardWord += str[reverseIteration];
+    }
+        
+    if (forwardWord !== backwardWord) {
+        isPalindrome = false;
+    }
+       
+    return isPalindrome;
 }
