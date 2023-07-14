ccef51482fcd44a8ce720982b054cdb76989c0c2
Emily Miller <emilyrauch3@gmail.com>
2023-07-13 21:18:55 -0400

    exercise M8 complete

--- content/challenges/medium/8 - Is Isogram/func.js
+++ content/challenges/medium/8 - Is Isogram/func.js
@@ -5,4 +5,22 @@
 //   - Ignore letter case (should not be case sensitive).
 //   - All test cases contain valid one word strings.
 module.exports = (str) => {
+    let isIsogram = true;
+    const lowerCaseString = str.toLowerCase();
+
+    for (let char = 0; char < lowerCaseString.length; char++) {
+
+        for (let compare = char + 1; compare < lowerCaseString.length; compare++ ) {
+
+            if (lowerCaseString[char] === lowerCaseString[compare]) {
+                isIsogram = false;
+            }     
+        }
+
+        if (!isIsogram) {
+            break;
+        }
+        
+    }
+    return isIsogram;
 }
