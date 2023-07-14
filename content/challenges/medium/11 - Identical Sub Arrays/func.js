5623d262afc05eeaf6f28ba4a52d31cb1064d209
Emily Miller <emilyrauch3@gmail.com>
2023-07-13 22:15:24 -0400

    exercise M11 complete

--- content/challenges/medium/11 - Identical Sub Arrays/func.js
+++ content/challenges/medium/11 - Identical Sub Arrays/func.js
@@ -1,7 +1,27 @@
-// Create a function that takes in a two-dimensional array and returns the number of sub-arrays with identical elements.
+// Create a function that takes in a two-dimensional array and 
+// returns the number of sub-arrays with identical elements.
 // e.g. [['#', '#'],[2,2,2]], both sub-arrays contain identical elements so this would return 2.
 // Notes: 
 //   - Single-element arrays count as (trivially) having identical elements.
+
 module.exports = (arr) => {
 
+    let count = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        const subArray = arr[i];
+        let isIdentical = true;
+
+        for (let j = 1; j < subArray.length; j++) {
+            if (subArray[j] !== subArray [0]) {
+                isIdentical = false;
+                break;
+            }
+        }
+
+        if (isIdentical) {
+            count++;
+        }
+    }
+    return count;
 }
