123fd467bc8c87c9b60d611b9fd30f98d721e91c
Emily Miller <emilyrauch3@gmail.com>
2023-07-15 14:46:42 -0400

    exercise M12 complete

--- content/challenges/medium/12 - Little Dictonary/func.js
+++ content/challenges/medium/12 - Little Dictonary/func.js
@@ -1,6 +1,27 @@
-// Create a function that takes in an initial word and filters out an array to contain words that start with the same letters as the initial word.
+// Create a function that takes in an initial word and filters 
+// out an array to contain words that start with the same letters as the initial word.
 // If none of the words match, return an empty array.
 // Keep the filtered array in the same relative order as the original array of words.
 
 module.exports = (initial, words) => {
+    let filteredArray = [];
+
+    for (let i = 0; i < words.length; i++) {
+        const word = words[i];
+        let isMatching = true;
+
+        for (let j = 0; j < initial.length; j++) {
+
+            if (word[j] !== initial[j] ) {
+                isMatching = false;
+                break;
+            }
+        }
+
+            if (isMatching) {
+                filteredArray.push(word);
+            }
+
+    }
+    return filteredArray;
 }
