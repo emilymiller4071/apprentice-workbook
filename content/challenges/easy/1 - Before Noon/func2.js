8e958b5467ff0cab6b01591433bc4131413cef6b
Emily Miller <emilyrauch3@gmail.com>
2023-07-16 23:20:47 -0400

    exercise E1 second round

--- content/challenges/easy/1 - Before Noon/func2.js
+++ content/challenges/easy/1 - Before Noon/func2.js
@@ -1,2 +1,10 @@
 // Check if the time is before noon
 // time is in military time 0-24
+
+module.exports = (hour) => {
+    let isBeforeNoon = hour < 12;
+
+    console.log(isBeforeNoon ? 'It is before noon.' : 'It is after noon.');
+
+    return isBeforeNoon;
+}
