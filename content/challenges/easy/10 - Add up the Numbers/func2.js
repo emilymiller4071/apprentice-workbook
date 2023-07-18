bfb0208961fc4e0b38a029f2edf2b4c4f7d45861
Emily Miller <emilyrauch3@gmail.com>
2023-07-17 22:46:21 -0400

    exercise E10 second round

--- content/challenges/easy/10 - Add up the Numbers/func2.js
+++ content/challenges/easy/10 - Add up the Numbers/func2.js
@@ -2,3 +2,12 @@
 Add up all the numbers from 1 to the number you passed to the function. 
 For example, if the input is 4 then your function should return 10 because 1 + 2 + 3 + 4 = 10. 
 USE FOR LOOP */
+
+/* 
+This approach uses Gauss' mathmatic formula. It is efficient because it 
+eliminates looping through all the numbers.
+*/
+
+module.exports = (num) => {
+    return num * (num + 1) / 2;
+}
