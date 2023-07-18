682473243706d49c2383735344d353300e75fc79
Emily Miller <emilyrauch3@gmail.com>
2023-07-17 21:14:54 -0400

    exercise E4 second round

--- content/challenges/easy/4 - Gamble/func2.js
+++ content/challenges/easy/4 - Gamble/func2.js
@@ -1,3 +1,9 @@
 // profitable gamble is a game that yields a positive net profit
 // net profit is calculated in the following manner: net profit = probaliity * prize - cost of playing
 // return true if you think you can win
+
+module.exports = (probabilityOfWinning, prize, costOfPlaying) => {
+    let profitableGamble = (probabilityOfWinning * prize - costOfPlaying) > 0;
+
+    return profitableGamble;
+}
