34801f153a85a2a0b5bd4a1dc75f3f7a4f6a03a3
Emily Miller <emilyrauch3@gmail.com>
2023-07-18 21:28:38 -0400

    exercise M1 second round

--- content/challenges/medium/1 - Double Letter/func2.js
+++ content/challenges/medium/1 - Double Letter/func2.js
@@ -1,2 +1,10 @@
 // Create a function that takes a word and returns true if the word has two 
 //consecutive identical letters.
+
+// This regular expression uses . to capture any letter
+// /1 refers to the captured letter so only consecutively repeating letters
+// trigger the boolean response of true when we use .test
+
+module.exports = (word) => {
+    return /(.)\1/.test(word);
+}
