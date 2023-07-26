7598eb9e91db93e52f80c30d3fd1c767fc0e95b2
Emily Miller <emilyrauch3@gmail.com>
2023-07-26 16:55:34 -0400

    exercise M5 second round

--- content/challenges/medium/5 - Stretched Words/func2.js
+++ content/challenges/medium/5 - Stretched Words/func2.js
@@ -2,3 +2,14 @@
 // duplicate consecutive letters removed.
 // Notes:
 //   - Final strings won't include words with double letters (e.g. "passing", "lottery").
+
+module.exports = (word) => {
+   const letters = word.toLowerCase().split('');
+   const filteredLetters = letters.filter((char, index) => {
+        if (char === 0) {
+            return true;
+        } 
+        return char !== letters[index - 1];
+   });
+   return filteredLetters.join('');
+}
